5b33a003c84dfa15ace42897a50dae0f943ff8c1
dpcks0919 <dpcks0919@gmail.com>
2023-12-13 11:26:13 +0900

    styling-react

---
+++ src/SaasComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SassComponent.scss'
+
+const SaasComponent = () => {
+    return (
+        <div className={"SaasComponent"}>
+            <div className={"box red"}/>
+            <div className={"box orange"}/>
+            <div className={"box yellow"}/>
+            <div className={"box green"}/>
+            <div className={"box blue"}/>
+            <div className={"box indigo"}/>
+            <div className={"box violet"}/>
+        </div>
+    );
+};
+
+export default SaasComponent;
